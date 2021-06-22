3f4fc0d3ea9f7ec9cc1ff21bc7e27bb63255f6e8
Cat-Denton <willwdenton@gmail.com>
2021-06-21 20:36:45 -0700

    add validation
    Co-authored-by: Saoud Rana <saoud@saoud.dev>
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>

--- TravelReviews/Models/Review.cs
+++ TravelReviews/Models/Review.cs
@@ -1,13 +1,24 @@
+using System.ComponentModel.DataAnnotations;
 namespace TravelReview.Models
 {
   public class Review
   {
     public int ReviewId { get; set; }
+
+    [Required]
+    [StringLength(50, ErrorMessage = "Title must be fewer than 50 characters long")]
     public string Title { get; set; }
+    [Required]
+    [StringLength(25)]
     public string City { get; set; }
+    [Required]
     public string Country { get; set; }
+    [Required]
     public string Author { get; set; }
+    [Required]
     public string Text { get; set; }
+    [Required]
+    [Range(0,5, ErrorMessage = "Rating must be between 0 and 5.")]
     public int Rating { get; set; }
   }
 }
